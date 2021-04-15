6fe58046bf0f60e62bc4a6de64c2792d1fe9766f
madhavwebsolution <developer.madhavtechnolabs@gmail.com>
2021-04-15 18:22:12 +0530

    add formik

--- src/components/Left.js
+++ src/components/Left.js
@@ -1,53 +1,57 @@
-import React from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Left = () => {
-  return (
-    <div className="left">
-      <div className="logo">
-        <img src="./images/Logo.png" alt="Logo" />
-      </div>
-      <div className="left-items">
-        <ul>
-          <li>
-            <img src="./images/icons/Vector.png" alt="Overview" />
-            <a href="#">Overview</a>
-          </li>
-          <li>
-            <img src="./images/icons/cloud.png" alt="Overview" />
-            <a href="#">Cloud</a>
-          </li>
-          <li>
-            <img src="./images/icons/sketch.png" alt="Overview" />
-            <a href="#">Sketch</a>
-          </li>
-          <li>
-            <img src="./images/icons/experment.png" alt="Overview" />
-            <a href="#">Experiment</a>
-          </li>
-          <li>
-            <img src="./images/icons/security.png" alt="Overview" />
-            <a href="#">Security</a>
-          </li>
-          <li>
-            <img src="./images/icons/owner.png" alt="Overview" />
-            <a href="#">Ownership</a>
-          </li>
-          <li>
-            <img src="./images/icons/test.png" alt="Overview" />
-            <a href="#">A/B Test</a>
-          </li>
-          <li>
-            <img src="./images/icons/colors.png" alt="Overview" />
-            <a href="#">Colors</a>
-          </li>
-        </ul>
-      </div>
-      <div className="logout">
-        <img src="./images/icons/logout.png" alt="logOut" />
-        <a href="#">Logout</a>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="left">
+            <div className="logo">
+                <img src="./images/Logo.png" alt="Logo" />
+            </div>
+            <div className="left-items">
+                <ul>
+                    <li>
+                        <img src="./images/icons/Vector.png" alt="Overview" />
+                        <Link to="/">Overview</Link>
+                    </li>
+                    <li>
+                        <img src="./images/icons/cloud.png" alt="Overview" />
+                        <Link to="/">Cloud</Link>
+                    </li>
+                    <li>
+                        <img src="./images/icons/sketch.png" alt="Overview" />
+                        <Link to="/">Sketch</Link>
+                    </li>
+                    <li>
+                        <img
+                            src="./images/icons/experment.png"
+                            alt="Overview"
+                        />
+                        <Link to="/">Experiment</Link>
+                    </li>
+                    <li>
+                        <img src="./images/icons/security.png" alt="Overview" />
+                        <Link to="/">Security</Link>
+                    </li>
+                    <li>
+                        <img src="./images/icons/owner.png" alt="Overview" />
+                        <Link to="/">Ownership</Link>
+                    </li>
+                    <li>
+                        <img src="./images/icons/test.png" alt="Overview" />
+                        <Link to="/">A/B Test</Link>
+                    </li>
+                    <li>
+                        <img src="./images/icons/colors.png" alt="Overview" />
+                        <Link to="/">Colors</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="logout">
+                <img src="./images/icons/logout.png" alt="logOut" />
+                <Link to="/">Logout</Link>
+            </div>
+        </div>
+    )
+}
 
-export default Left;
+export default Left
--- src/components/UserAdd.js
+++ src/components/UserAdd.js
@@ -1,78 +1,178 @@
 import React from 'react'
+import { IoIosArrowRoundBack } from 'react-icons/io'
+import { Link } from 'react-router-dom'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+const addUserSchema = Yup.object().shape({
+    fname: Yup.string().required(),
+    lname: Yup.string().required(),
+    email: Yup.string().email().required(),
+    dob: Yup.string().required(),
+    phone: Yup.number().required(),
+    education: Yup.string().required(),
+    address: Yup.string().required(),
+})
 
 const UserAdd = () => {
     return (
         <>
             <div>
                 <div className="header">
-                    <h2>Add User</h2>
-                    <div className="btns" />
+                    <h2>
+                        <Link to="/">
+                            <IoIosArrowRoundBack
+                                style={{
+                                    verticalAlign: 'middle',
+                                    width: '40px',
+                                    height: '40px',
+                                    color: '#414141',
+                                    marginRight: '1rem',
+                                }}
+                            />
+                        </Link>
+                        Add User
+                    </h2>
+                    <div className="btns"></div>
                 </div>
                 <main>
-                    <div className="form-container">
-                        <div className="form-group">
-                            <label htmlFor="name">First Name</label>
-                            <input
-                                className="form-element"
-                                type="text"
-                                id="fname"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="name">Last Name</label>
-                            <input
-                                className="form-element"
-                                type="text"
-                                id="lname"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="name">Date Of Birth</label>
-                            <input
-                                className="form-element"
-                                type="date"
-                                id="fname"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="name">Phone</label>
-                            <input
-                                className="form-element"
-                                type="text"
-                                id="lname"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="name">Email</label>
-                            <input
-                                className="form-element"
-                                type="text"
-                                id="lname"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="name">Education</label>
-                            <select nam className="form-element" e id>
-                                <option value={1}>Bsc (IT)</option>
-                                <option value={2}>BE</option>
-                                <option value={3}>BCA</option>
-                            </select>
-                        </div>
-                        <div className="form-group span-2">
-                            <label htmlFor="name">Address</label>
-                            <textarea
-                                className="textarea form-element"
-                                name
-                                id
-                                cols={30}
-                                rows={10}
-                                defaultValue={''}
-                            />
-                        </div>
-                        <div className="form-group submit-btn span-2">
-                            <input type="submit" defaultValue="submit" />
-                        </div>
-                    </div>
+                    <Formik
+                        initialValues={{
+                            fname: '',
+                            lname: '',
+                            email: '',
+                            dob: '',
+                            phone: '',
+                            education: '',
+                            address: '',
+                        }}
+                        validationSchema={addUserSchema}
+                        onSubmit={(values) => {
+                            // same shape as initial values
+                            console.log(values)
+                        }}
+                    >
+                        {({ errors, touched }) => (
+                            <Form>
+                                <div className="form-container">
+                                    <div className="form-group">
+                                        <label htmlFor="name">First Name</label>
+                                        <Field
+                                            className="form-element"
+                                            type="text"
+                                            name="fname"
+                                            id="fname"
+                                        />
+                                        <ErrorMessage
+                                            name="fname"
+                                            component="span"
+                                            className="error"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="name">Last Name</label>
+                                        <Field
+                                            className="form-element"
+                                            type="text"
+                                            id="lname"
+                                            name="lname"
+                                        />
+                                        <ErrorMessage
+                                            name="lname"
+                                            component="span"
+                                            className="error"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="dob">
+                                            Date Of Birth
+                                        </label>
+                                        <Field
+                                            className="form-element"
+                                            type="date"
+                                            name="dob"
+                                            id="dob"
+                                        />
+                                        <ErrorMessage
+                                            name="dob"
+                                            component="span"
+                                            className="error"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="phone">Phone</label>
+                                        <Field
+                                            className="form-element"
+                                            type="text"
+                                            id="phone"
+                                            name="phone"
+                                        />
+                                        <ErrorMessage
+                                            name="phone"
+                                            component="span"
+                                            className="error"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="name">Email</label>
+                                        <Field
+                                            className="form-element"
+                                            type="text"
+                                            id="email"
+                                            name="email"
+                                        />
+                                        <ErrorMessage
+                                            name="email"
+                                            component="span"
+                                            className="error"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="education">
+                                            Education
+                                        </label>
+                                        <Field
+                                            as="select"
+                                            name="education"
+                                            className="form-element"
+                                            id="education"
+                                        >
+                                            <option value="">- select -</option>
+                                            <option value={1}>Bsc (IT)</option>
+                                            <option value={2}>BE</option>
+                                            <option value={3}>BCA</option>
+                                        </Field>
+                                        <ErrorMessage
+                                            name="education"
+                                            component="span"
+                                            className="error"
+                                        />
+                                    </div>
+                                    <div className="form-group span-2">
+                                        <label htmlFor="address">Address</label>
+                                        <Field
+                                            as="textarea"
+                                            className="textarea form-element"
+                                            name="address"
+                                            id="address"
+                                        />
+                                        <ErrorMessage
+                                            name="address"
+                                            component="span"
+                                            className="error"
+                                        />
+                                    </div>
+                                    <div className="form-group submit-btn span-2">
+                                        <Field
+                                            name="submit"
+                                            type="submit"
+                                            defaultValue="submit"
+                                        />
+                                    </div>
+                                </div>
+                            </Form>
+                        )}
+                    </Formik>
                 </main>
             </div>
         </>
